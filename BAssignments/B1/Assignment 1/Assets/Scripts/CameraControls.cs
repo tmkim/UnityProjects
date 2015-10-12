0ba5d2991f7fc3f8e0c37118a18f4ce8e1846c3b
owlsketch <satimidus@gmail.com>
2015-10-12 16:53:42 -0400

    Camera controls and raycaster implemented.

---
+++ BAssignments/B1/Assignment 1/Assets/Scripts/CameraControls.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControls : MonoBehaviour {
+    public float speed;
+    private Vector3 mousePosition;
+
+	void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        //if left click and cursor isn't locked
+        if (Input.GetMouseButtonDown(0) && Cursor.lockState != CursorLockMode.Locked)
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+    }
+
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveForward = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveForward);
+        transform.Translate(movement * speed);
+
+        if (Cursor.lockState == CursorLockMode.Locked)
+        {
+            //Rotation
+            //yaw rotation
+            transform.Rotate(0, Input.GetAxis("Mouse X"), 0);
+            //pitch
+            gameObject.transform.GetChild(0).transform.Rotate(-Input.GetAxis("Mouse Y"), 0, 0);
+        }
+    }
+}
---
+++ BAssignments/B1/Assignment 1/Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraZoom : MonoBehaviour {
+    public Camera cam;
+    private Vector2 deltaMouseScroll;
+    private RaycastHit hit;
+    private GameObject cursor;
+
+    // Use this for initialization
+    void Start () {
+        cursor = GameObject.Find("Cursor");
+	}
+
+    // Update is called once per frame
+    void Update() {
+        //RayCast
+        //emit a ray from exactly the middle of the screen
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0.0f));
+        //if there was an object intersected
+        if (Physics.Raycast(ray, out hit))
+        {
+            cursor.transform.localScale = new Vector3(0.3f, 0.3f);
+            //hit is a reference to the object we intersected with
+        }
+        else
+        {
+            cursor.transform.localScale = new Vector3(0.2f, 0.2f);
+        }
+
+        //Zooming:
+        deltaMouseScroll = Input.mouseScrollDelta;
+        cam.fieldOfView -= deltaMouseScroll.y * 2;
+        if (cam.fieldOfView < 15f)
+        {
+            cam.fieldOfView = 15f;
+        }
+        if (cam.fieldOfView > 100f)
+        {
+            cam.fieldOfView = 100f;
+        }
+
+    }
+}
